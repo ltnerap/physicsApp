9a99f5946f5ef7fb884036a910584cfe9d0fbbf9
gitForAI <lp4390a@student.american.edu>
2015-04-23 13:57:02 -0400

    app is finished and working

--- MyView.java
+++ MyView.java
@@ -26,7 +26,9 @@ public class MyView extends View implements View.OnTouchListener, GestureDetecto
     boolean stopFound = false;
     Bitmap table;
     private GestureDetector detect;
-    public int s;
+    public float s;
+    boolean drawing = false;
+    int draw = 0;
 
     public MyView(Context context) {
         super(context);
@@ -52,7 +54,9 @@ public class MyView extends View implements View.OnTouchListener, GestureDetecto
     }
 
     public void onLongPress(MotionEvent e){
+
         InitActivity.isPaused = !InitActivity.isPaused;
+        draw = 0;
     }
 
     public boolean onFling(MotionEvent e1, MotionEvent e2, float start, float end){
@@ -63,16 +67,10 @@ public class MyView extends View implements View.OnTouchListener, GestureDetecto
         detect.onTouchEvent(e);
         switch(e.getAction()){
             case MotionEvent.ACTION_DOWN:
-                break;
-            case MotionEvent.ACTION_UP:
-                if(!InitActivity.isPaused) {
-                    InitActivity.balls[InitActivity.BallCount] = new Ball(e.getX()*(s/InitActivity.WIDTH), e.getY()/(s/InitActivity.HEIGHT));
-                    this.postInvalidate();
-                    InitActivity.BallCount++;
-                }
-                else if(InitActivity.isPaused){
-                    startX = (int)e.getX()*(s/InitActivity.WIDTH);
-                    startY = (int)e.getY()/(s/InitActivity.HEIGHT);
+                System.out.println("made it here");
+                if(InitActivity.isPaused) {
+                    startX = (int)(e.getX()/s);
+                    startY = (int)(e.getY()/s);
                     for(int i=0; i<InitActivity.BallCount; i++){
                         if(InitActivity.balls[i].contains((float)startX, (float)startY)){
                             startFound = true;
@@ -81,9 +79,20 @@ public class MyView extends View implements View.OnTouchListener, GestureDetecto
                         }
                     }
                 }
+                else {
+                    break;
+                }
+                break;
+            case MotionEvent.ACTION_UP:
+                if(!InitActivity.isPaused) {
+                    InitActivity.balls[InitActivity.BallCount] = new Ball(e.getX()/s, e.getY()/s);
+                    this.postInvalidate();
+                    InitActivity.BallCount++;
+                }
+
                 else if(startFound && InitActivity.isPaused){
-                    stopX = (int)e.getX()*(s/InitActivity.WIDTH);
-                    stopY = (int)e.getY()*(s/InitActivity.HEIGHT);
+                    stopX = (int)(e.getX()/s);
+                    stopY = (int)(e.getY()/s);
                     for(int i=0; i<InitActivity.BallCount; i++){
                         if(InitActivity.balls[i].contains((float)stopX, (float)stopY) && startBall != i){
                             stopFound = true;
@@ -97,30 +106,52 @@ public class MyView extends View implements View.OnTouchListener, GestureDetecto
                     startFound = false;
                     stopFound = false;
                     InitActivity.LinkCount++;
-                    this.invalidate();
+                    this.postInvalidate();
                 }
-                else if(InitActivity.isPaused && !startFound && !stopFound){
-                    InitActivity.walls[InitActivity.WallCount] = new Wall(startX, startY, stopX, stopY);
-                    InitActivity.WallCount++;
-                    this.invalidate();
+                else if(InitActivity.isPaused && draw != 0){
+                    stopX = (int)(e.getX()/s);
+                    stopY = (int)(e.getY()/s);
+                    System.out.println("startx = " + startX + ", starty = " + startY + ", stopx = " + stopX + ", stopy = " + stopY);
+                    if(startX != stopX || startY != stopY) {
+                        System.out.println("here");
+                        InitActivity.walls[InitActivity.WallCount] = new Wall(startX, startY, stopX, stopY);
+                        InitActivity.WallCount++;
+                        this.postInvalidate();
+                    }
                 }
+                if(InitActivity.isPaused){
+                    draw = 1;
+                }
+                break;
+            case MotionEvent.ACTION_MOVE:
+                if(InitActivity.isPaused && !startFound && !stopFound){
+                    //stopX = (int)(e.getX()/s);
+                    //stopY = (int)(e.getY()/s);
+                    //System.out.println("startx = " + startX + ", starty = " + startY + ", stopx = " + stopX + ", stopy = " + stopY);
+                    /*InitActivity.walls[InitActivity.WallCount] = new Wall(startX, startY, stopX, stopY);
+                    InitActivity.WallCount++;*/
+                    this.postInvalidate();
+                    drawing = true;
+                    break;
+                }
+                break;
         }
         return true;
     }
 
     public void onDraw(Canvas g){
-        int w = g.getWidth();
-        int h = g.getHeight();
-        if(w < h){
-            s = w;
+        float w = g.getWidth();
+        float h = g.getHeight();
+        if(w/InitActivity.WIDTH < h/InitActivity.HEIGHT){
+            s = w/InitActivity.WIDTH;
         }
         else {
-            s = h;
+            s = h/InitActivity.HEIGHT;
         }
-        g.scale((float)s/InitActivity.WIDTH, (float)s/InitActivity.HEIGHT);
+        g.scale(s, s);
         Paint paint = new Paint();
         paint.setColor(Color.WHITE);
-        g.drawRect(0,0, (float)s, (float)s, paint);
+        g.drawRect(0,0, InitActivity.WIDTH, InitActivity.HEIGHT, paint);
         for(int i=0; i<InitActivity.BallCount; i++){
             InitActivity.balls[i].draw(g);
         }
